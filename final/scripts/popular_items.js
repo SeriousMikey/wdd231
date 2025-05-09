cc23eb4c0bf3b868e776398e91f713b60db9b061
Michael Hyer <michaelhy2002@gmail.com>
2025-05-08 22:23:22 -0600

    finish home page final

--- final/scripts/popular_items.js
+++ final/scripts/popular_items.js
@@ -1,24 +1,25 @@
-//let linksURL = "https://seriousmikey.github.io/wdd231/final/data/members.json";
-let linksURL = "data/items.json";
+//let linksURL = "https://seriousmikey.github.io/wdd231/final/data/catalog.json";
+let linksURL = "data/catalog.json";
 const popularItemsContainer = document.getElementById("popularItemsContainer");
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
 
-    populateItems(data.members);
+    populateItems(data.items);
 }
 
 function populateItems(items) {
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 3; i++) {
         let div = document.createElement("div");
-        div.setAttribute("class", "popularItem");
+        div.setAttribute("class", "item");
 
         div.innerHTML = `
+        <a href="catalog.html">
             <img src="${items[i].image}" alt="${items[i].name}">
-            <p>${items[i].name}</p>
-            `
-        
+            <p><i>${items[i].name}</i></p>
+        </a>
+        `
             popularItemsContainer.appendChild(div);
     }
 }
